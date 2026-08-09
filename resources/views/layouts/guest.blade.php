4e47ebef07e596b1bbaaf87c6f9764482f04e686
Léo Cunha <leocunha.jobs@gmail.com>
2026-08-09 01:17:27 -0300

    Fix invisible brand wordmark on the login page in dark mode
    <x-brand-logo> was given a hardcoded text-gray-900 with no dark:
    counterpart, so the wordmark text disappeared against the guest
    layout's dark:bg-gray-900 background when the OS is in dark mode.

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -18,7 +18,7 @@
         <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100 dark:bg-gray-900">
             <div>
                 <a href="/" wire:navigate>
-                    <x-brand-logo class="h-14 w-auto text-gray-900" />
+                    <x-brand-logo class="h-14 w-auto text-gray-900 dark:text-white" />
                 </a>
             </div>
 
